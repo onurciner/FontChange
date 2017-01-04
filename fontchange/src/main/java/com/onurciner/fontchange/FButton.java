672f0a8f7e9e1f3e3441f2437fb43976b1e95c89
10uroi <onur_ciner_200@hotmail.com>
2017-01-04 13:32:36 +0300

    v1.2

--- fontchange/src/main/java/com/onurciner/fontchange/FButton.java
+++ fontchange/src/main/java/com/onurciner/fontchange/FButton.java
@@ -14,6 +14,7 @@ public class FButton extends Button {
 
     String font = null;
     Integer style = 0;
+    Integer family = 0;
 
     public FButton(Context context, AttributeSet attrs) {
         super(context, attrs);
@@ -26,23 +27,63 @@ public class FButton extends Button {
         try {
             font = a.getString(R.styleable.FButton_fontButton);
             style = a.getInteger(R.styleable.FButton_fontStyleButton, 0);
+            family = a.getInteger(R.styleable.FButton_fontFamilyButton, 0);
         } finally {
             a.recycle();
         }
 
-        if (font != null) {
-            Typeface tf = Typeface.createFromAsset(context.getAssets(), font);
-            if (style == 0)
-                this.setTypeface(tf, Typeface.NORMAL);
-            else if (style == 1)
-                this.setTypeface(tf, Typeface.ITALIC);
-            else if (style == 2)
-                this.setTypeface(tf, Typeface.BOLD);
-            else if (style == 3)
-                this.setTypeface(tf, Typeface.BOLD_ITALIC);
-            else
-                this.setTypeface(tf);
+        Typeface tf = null;
+        if(family == 1){
+            tf = Typeface.createFromAsset(context.getAssets(), "alien_encounters.ttf");
+        }else if(family == 2){
+            tf = Typeface.createFromAsset(context.getAssets(), "all_ages.ttf");
+        }else if(family == 3){
+            tf = Typeface.createFromAsset(context.getAssets(), "android_style.ttf");
+        }else if(family == 4){
+            tf = Typeface.createFromAsset(context.getAssets(), "black_and_white.ttf");
+        }else if(family == 5){
+            tf = Typeface.createFromAsset(context.getAssets(), "bright_larch.ttf");
+        }else if(family == 6){
+            tf = Typeface.createFromAsset(context.getAssets(), "earth_orbiter.ttf");
+        }else if(family == 7){
+            tf = Typeface.createFromAsset(context.getAssets(), "friday13.ttf");
+        }else if(family == 8){
+            tf = Typeface.createFromAsset(context.getAssets(), "led_display_st.ttf");
+        }else if(family == 9){
+            tf = Typeface.createFromAsset(context.getAssets(), "neuropol.ttf");
+        }else if(family == 10){
+            tf = Typeface.createFromAsset(context.getAssets(), "painter.ttf");
+        }else if(family == 11){
+            tf = Typeface.createFromAsset(context.getAssets(), "prismfont.ttf");
+        }else if(family == 12){
+            tf = Typeface.createFromAsset(context.getAssets(), "sheeping_dogs.ttf");
+        }else if(family == 13){
+            tf = Typeface.createFromAsset(context.getAssets(), "star_jedi.ttf");
+        }else if(family == 14){
+            tf = Typeface.createFromAsset(context.getAssets(), "top_secret.ttf");
+        }else if(family == 15){
+            tf = Typeface.createFromAsset(context.getAssets(), "caviar_dreams.ttf");
+        }else if(family == 16){
+            tf = Typeface.createFromAsset(context.getAssets(), "champagne_limousines.ttf");
+        }else{
+            tf = Typeface.createFromAsset(context.getAssets(), "champagne_limousines.ttf");
         }
+
+        if (font != null) {
+            tf = Typeface.createFromAsset(context.getAssets(), font);
+        }
+
+        if (style == 0)
+            this.setTypeface(tf, Typeface.NORMAL);
+        else if (style == 1)
+            this.setTypeface(tf, Typeface.ITALIC);
+        else if (style == 2)
+            this.setTypeface(tf, Typeface.BOLD);
+        else if (style == 3)
+            this.setTypeface(tf, Typeface.BOLD_ITALIC);
+        else
+            this.setTypeface(tf);
+
 
     }
 
